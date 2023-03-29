238181702a2919d7d5c3f63197c229dc0ffcbb9f
Malcolm Kahora <mango22mjk@gmail.com>
2023-03-29 10:41:59 -0400

    Octogons rendering working

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use bevy_prototype_lyon::prelude::*;
+use bevy::prelude::*;
+
+// Draw a point on the screen
+
+fn main() {
+
+    App::new()
+        .insert_resource(Msaa::Sample4)
+	.add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
+	.add_startup_system(setup_system).run();
+
+}
+
+
+fn setup_system(mut commands: Commands) {
+    let shape = shapes::RegularPolygon {
+        sides: 6,
+        feature: shapes::RegularPolygonFeature::Radius(200.0),
+        ..shapes::RegularPolygon::default()
+    };
+
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        ShapeBundle {
+            path: GeometryBuilder::build_as(&shape),
+            ..default()
+        },
+        Fill::color(Color::RED),
+        Stroke::new(Color::BLACK, 10.0),
+    ));
+}
