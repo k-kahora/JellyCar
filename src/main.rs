eb4dc97d6c5d8272aa385e3fd179d9d6393b2283
Malcolm Kahora <mango22mjk@gmail.com>
2023-03-29 14:19:54 -0400

    System runs in paralell so they are independant

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::f64::consts::PI;
-
 use bevy::prelude::*;
-use bevy_prototype_lyon::{entity::ShapeBundle, prelude::*};
+use bevy_prototype_lyon::prelude::*;
 
 fn main() {
     App::new()
@@ -9,62 +7,101 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(ShapePlugin)
         .add_startup_system(setup_system)
-        .add_system(change_draw_mode_system)
-        .add_system(change_number_of_sides)
-        .add_system(rotate_shape_system)
+        .add_system(spawn_point)
         .run();
 }
 
 #[derive(Component)]
-struct ExampleShape;
+struct PointGroup {
+    
+    pub points: [Vec2;5],
 
-fn rotate_shape_system(mut query: Query<&mut Transform, With<ExampleShape>>, time: Res<Time>) {
-    let delta = time.delta_seconds();
-
-    for mut transform in query.iter_mut() {
-        transform.rotate(Quat::from_rotation_z(0.2 * delta));
-    }
 }
 
-fn change_draw_mode_system(mut query: Query<(&mut Fill, &mut Stroke)>, time: Res<Time>) {
-    let hue = (time.elapsed_seconds_f64() * 50.0) % 360.0;
-    let outline_width = 2.0 + time.elapsed_seconds_f64().sin().abs() * 10.0;
+fn spawn_point
+    (
+	mut commands: Commands,
+	point_group_query: Query<&PointGroup>,
 
-    for (mut fill_mode, mut stroke_mode) in query.iter_mut() {
-        fill_mode.color = Color::hsl(hue as f32, 1.0, 0.5);
-        stroke_mode.options.line_width = outline_width as f32;
-    }
-}
+    ) {
+	println!("{:?}", point_group_query);
 
-fn change_number_of_sides(mut query: Query<&mut Path>, time: Res<Time>) {
-    let sides = ((time.elapsed_seconds_f64() - PI * 2.5).sin() * 2.5 + 5.5).round() as usize;
+	for group in point_group_query.iter() {
+	    for point in group.points {
+		let circle = shapes::Circle {
+		    radius: 10.,
+		    center: point.clone(),
+		};
+		println!("Hree");
 
-    for mut path in query.iter_mut() {
-        let polygon = shapes::RegularPolygon {
-            sides,
-            feature: shapes::RegularPolygonFeature::Radius(200.0),
-            ..shapes::RegularPolygon::default()
-        };
+		commands.spawn(
+		    (
+			ShapeBundle {
+			    path: GeometryBuilder::build_as(&circle),
+			    ..default()
+			},
+			Fill::color(Color::CYAN),
 
-        *path = ShapePath::build_as(&polygon);
-    }
+		    )
+
+		);
+	    }
+	}
+
+
 }
 
 fn setup_system(mut commands: Commands) {
-    let shape = shapes::RegularPolygon {
-        sides: 6,
-        feature: shapes::RegularPolygonFeature::Radius(200.0),
-        ..shapes::RegularPolygon::default()
-    };
+    
+    let mut path_builder = PathBuilder::new();
+    path_builder.move_to(Vec2::new(0., 0.));
+    path_builder.cubic_bezier_to(
+        Vec2::new(70., 70.),
+        Vec2::new(175., -35.),
+        Vec2::new(0., -140.),
+    );
+    path_builder.cubic_bezier_to(
+        Vec2::new(-175., -35.),
+        Vec2::new(-70., 70.),
+        Vec2::new(0., 0.),
+    );
+    path_builder.close();
+    let path = path_builder.build();
 
+    commands.spawn(PointGroup {
+	points: [
+	    Vec2::new(10.,30.),
+	    Vec2::new(30.,30.),
+	    Vec2::new(30.,30.),
+	    Vec2::new(200.,30.),
+	    Vec2::new(0.,30.),
+	]
+    });
     commands.spawn(Camera2dBundle::default());
-    commands.spawn((
-        ShapeBundle {
-            path: GeometryBuilder::build_as(&shape),
-            ..default()
-        },
-        Fill::color(Color::CYAN),
-        Stroke::new(Color::BLACK, 10.0),
-        ExampleShape,
-    ));
+
+    // let circle = shapes::Circle {
+    // 	radius: 32.,
+    // 	center: Vec2::new(200., 21.),
+    // };
+
+    // commands.spawn(
+    // 	(
+    // 	    ShapeBundle {
+    // 		path: GeometryBuilder::build_as(&circle),
+    // 		..default()
+    // 	    },
+    // 	    Fill::color(Color::CYAN),
+
+    // 	)
+
+    // );
+    // commands.spawn((
+    //     ShapeBundle {
+    //         path,
+    //         transform: Transform::from_xyz(0., 75., 0.),
+    //         ..default()
+    //     },
+    //     Stroke::new(Color::BLACK, 10.0),
+    //     Fill::color(Color::RED),
+    // ));
 }
