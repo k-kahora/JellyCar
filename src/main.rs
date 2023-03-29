8116f0eca2be9e08fd6942106fb78790cd45aeda
Malcolm Kahora <mango22mjk@gmail.com>
2023-03-29 12:19:58 -0400

    Changing shapes with color and sides

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,59 @@
-use bevy_prototype_lyon::prelude::*;
-use bevy::prelude::*;
+use std::f64::consts::PI;
 
-// Draw a point on the screen
+use bevy::prelude::*;
+use bevy_prototype_lyon::{entity::ShapeBundle, prelude::*};
 
 fn main() {
-
     App::new()
         .insert_resource(Msaa::Sample4)
-	.add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins)
         .add_plugin(ShapePlugin)
-	.add_startup_system(setup_system).run();
-
+        .add_startup_system(setup_system)
+        .add_system(change_draw_mode_system)
+        .add_system(change_number_of_sides)
+        .add_system(rotate_shape_system)
+        .run();
 }
 
+#[derive(Component)]
+struct ExampleShape;
+
+fn rotate_shape_system(mut query: Query<&mut Transform, With<ExampleShape>>, time: Res<Time>) {
+    let delta = time.delta_seconds();
+
+    for mut transform in query.iter_mut() {
+        transform.rotate(Quat::from_rotation_z(0.2 * delta));
+    }
+}
+
+fn change_draw_mode_system(mut query: Query<(&mut Fill, &mut Stroke)>, time: Res<Time>) {
+    let hue = (time.elapsed_seconds_f64() * 50.0) % 360.0;
+    let outline_width = 2.0 + time.elapsed_seconds_f64().sin().abs() * 10.0;
+
+    for (mut fill_mode, mut stroke_mode) in query.iter_mut() {
+        fill_mode.color = Color::hsl(hue as f32, 1.0, 0.5);
+        stroke_mode.options.line_width = outline_width as f32;
+    }
+}
+
+fn change_number_of_sides(mut query: Query<&mut Path>, time: Res<Time>) {
+    let sides = ((time.elapsed_seconds_f64() - PI * 2.5).sin() * 2.5 + 5.5).round() as usize;
+
+    for mut path in query.iter_mut() {
+        let polygon = shapes::RegularPolygon {
+            sides,
+            feature: shapes::RegularPolygonFeature::Radius(200.0),
+            ..shapes::RegularPolygon::default()
+        };
+
+        *path = ShapePath::build_as(&polygon);
+    }
+}
 
 fn setup_system(mut commands: Commands) {
     let shape = shapes::RegularPolygon {
-        sides: 30,
-        feature: shapes::RegularPolygonFeature::Radius(400.0),
+        sides: 6,
+        feature: shapes::RegularPolygonFeature::Radius(200.0),
         ..shapes::RegularPolygon::default()
     };
 
@@ -27,7 +63,8 @@ fn setup_system(mut commands: Commands) {
             path: GeometryBuilder::build_as(&shape),
             ..default()
         },
-        Fill::color(Color::RED),
+        Fill::color(Color::CYAN),
         Stroke::new(Color::BLACK, 10.0),
+        ExampleShape,
     ));
 }
