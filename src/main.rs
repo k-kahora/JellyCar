ddfb3ab50e3a1a0130629a6b572f2d486b046214
Malcolm Kahora <mango22mjk@gmail.com>
2023-04-07 00:25:13 -0400

    Bounding Box Structure

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,9 @@ pub const GRAVITY: f32 = 9.;
 struct Position(Vec2);
 
 #[derive(Component)]
+struct Point;
+
+#[derive(Component)]
 struct Direction(Vec2);
 
 // TODO Derive a speed compontent and use it to make the speed of eack poit independat
@@ -38,14 +41,44 @@ struct ObjectName(String);
 #[derive(Component)]
 struct Mass(i32);
 
+#[derive(Component)]
+struct Force(Vec2);
+
+#[derive(Component)]
+struct DampingFactor(i32);
+
 // We have a object this object is a entity with the name Car
 // The car has a buck of points associated with it that has owners
 
 #[derive(Bundle)]
-struct MassPointGroup {
-    name: ObjectName,
-    direction: Direction
+struct Spring {
+    // Two points
+    // Stiffness
+    // Rest length
+    // Dampang Factor
 }
+#[derive(Component)]
+struct BoundingBox {
+    bounding_square: Vec<Vec2>,
+}
+
+impl Default for BoundingBox {
+    fn default() -> Self {
+	BoundingBox { bounding_square: vec![
+	    Vec2::new(0., 0.),
+	    Vec2::new(0., 1.),
+	    Vec2::new(1., 1.),
+	    Vec2::new(1., 0.),
+	    Vec2::new(0., 0.),
+	    ]
+	}
+    }
+}
+
+#[derive(Bundle)]
+struct MassPointGroup {
+}
+
 #[derive(Component)]
 struct Group;
 
@@ -55,14 +88,13 @@ struct PointMassBundle {
     mass: Mass,
     position: Position,
     direction: Direction,
-    shape: ShapeBundle,
-    owner: ObjectName,
-    color: Fill,
+    // Later replace speed with force
     speed: Speed,
-}
 
-#[derive(Component)]
-struct Point;
+    // Superflous data
+    shape: ShapeBundle,
+    color: Fill,
+}
 
 impl MassPointGroup {
     fn new_group(list_of_points: &Vec<Vec2>) -> Vec<PointMassBundle> {
@@ -86,7 +118,6 @@ impl MassPointGroup {
                     ..default()
                 },
                 // in the future get the name from MassPointgroup
-                owner: ObjectName("Square".to_string()),
                 color: Fill::color(Color::WHITE),
 		speed: Speed(0.),
             })
@@ -116,6 +147,16 @@ impl MassPointGroup {
 
 // The line is the parent and the points are the children
 // Query children in the line query
+
+fn minimum_bounding_box(
+    point_query: Query<&Transform, With<Point>>,
+    mut line_query: Query<(&mut Path, &Children), With<Group>>,
+    time: Res<Time>
+)
+{
+
+}
+
 fn line_movement(
     point_query: Query<&Transform, With<Point>>,
     mut line_query: Query<(&mut Path, &Children), With<Group>>,
@@ -176,10 +217,6 @@ fn startup_sequence(mut commands: Commands) {
     commands.spawn((
         paths,
         Stroke::new(Color::WHITE, 4.0),
-        MassPointGroup {
-            name: ObjectName("car".to_string()),
-            direction: Direction(Vec2::new(0., -1.)),
-        },
 	Group
     )).with_children(|parent| {
 		     for point in points {
@@ -190,11 +227,8 @@ fn startup_sequence(mut commands: Commands) {
     commands.spawn((
         square_lines,
         Stroke::new(Color::WHITE, 4.0),
-        MassPointGroup {
-            name: ObjectName("car".to_string()),
-            direction: Direction(Vec2::new(0., -1.)),
-        },
-	Group
+	Group,
+	BoundingBox::default()
     )).with_children(|parent| {
 		     for point in square_points {
 			 parent.spawn((point, Point));
